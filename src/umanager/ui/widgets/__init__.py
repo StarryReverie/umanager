ede280a83e129e7c77d64e9cfc06ef0d8bfd286a
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-12-26 12:20:35 +0800

    style: force multiline re-export

--- src/umanager/ui/widgets/__init__.py
+++ src/umanager/ui/widgets/__init__.py
@@ -3,4 +3,9 @@ from .file_manager_list_widget import FileManagerListWidget
 from .overview_button_bar import OverviewButtonBar
 from .overview_title_bar import OverviewTitleBar
 
-__all__ = ["DeviceInfoListWidget", "FileManagerListWidget", "OverviewButtonBar", "OverviewTitleBar"]
+__all__ = [
+    "DeviceInfoListWidget",
+    "FileManagerListWidget",
+    "OverviewButtonBar",
+    "OverviewTitleBar",
+]
